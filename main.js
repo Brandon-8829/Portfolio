5f7c68761be94337814ef583b14d144655a80b06
Brandon-8829 <58235449+Brandon-8829@users.noreply.github.com>
2020-08-09 18:00:06 -0400

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-  funciton sendEmail(){
-  	window.open('mailto:test@example.com');
-  }
-
-
-  function topFunction() {
-  	document.body.scrollTop = 0; // For Safari
-  	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
