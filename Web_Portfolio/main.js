dac13800883084e15ddfdf9d1074af1e9125eea0
Brandon-8829 <58235449+Brandon-8829@users.noreply.github.com>
2020-08-09 17:58:06 -0400

    Add files via upload

---
+++ Web_Portfolio/main.js
@@ -0,0 +1,9 @@
+  funciton sendEmail(){
+  	window.open('mailto:test@example.com');
+  }
+
+
+  function topFunction() {
+  	document.body.scrollTop = 0; // For Safari
+  	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
